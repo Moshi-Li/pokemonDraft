c4feca5500a835cc6f8762d9699d1c929e62a9d2
Moshi Li <li.moshi95@gmail.com>
2023-01-22 20:59:14 -0500

    feat added html for poke auction

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import "./header-elem/header-elem";
 import "./nav-bar-elem/nav-bar-elem";
 import "./article-elem/article-elem";
 import "./about-elem/about-elem";
+import "./poke-card-elem/poke-card-elem";
+import "./poke-auction-elem/poke-auction-elem";
---
+++ src/poke-card-elem/poke-card-elem.ts
@@ -0,0 +1,88 @@
+import { LitElement, html, customElement, property, state } from "lit-element";
+import Axios from "axios";
+
+interface PokeStateI {
+  key: string;
+  value: any;
+}
+
+@customElement("poke-card-elem")
+export class PokeCardElem extends LitElement {
+  @state()
+  private imageLink: string = "";
+  @state()
+  private pokeStats: Array<PokeStateI> = [];
+  @property({ type: Number }) pokeIndexNumber: number = 0;
+
+  async load() {
+    try {
+      const response = await Axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${this.pokeIndexNumber}`
+      );
+      const { name, stats, id } = response.data;
+
+      const nextPokeStats = [
+        { key: "name", value: name },
+        { key: "id", value: id },
+      ];
+      stats.forEach((state: any) => {
+        nextPokeStats.push({ key: state.stat.name, value: state.base_stat });
+      });
+
+      this.imageLink = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokeIndexNumber}.png`;
+      this.pokeStats = nextPokeStats;
+    } catch (e) {
+      console.log("error while loading");
+    }
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+    await this.load();
+  }
+
+  render() {
+    return html`
+      <style>
+        .poke--card {
+          background-color: transparent;
+          width: 250px;
+          padding: 20px;
+          border: 1px solid black;
+        }
+        .poke--card img {
+          width: 80%;
+          background-color: white;
+        }
+
+        .poke--info--container {
+          width: 100%;
+          margin-top: 6px;
+          border: 1px solid black;
+          padding: 6px;
+
+          display: flex;
+          flex-direction: column;
+
+          justify-content: space-between;
+        }
+        .poke--info--stat {
+          width: 70%;
+          margin: 4px;
+        }
+      </style>
+
+      <div class="poke--card">
+        <img src="${this.imageLink}" />
+
+        <div class="poke--info--container">
+          ${this.pokeStats.map(
+            (state: PokeStateI) => html`<div class="poke--info--stat">
+              <span>${state.key}: </span><span>${state.value}</span>
+            </div>`
+          )}
+        </div>
+      </div>
+    `;
+  }
+}
