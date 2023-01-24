5d58b1d9390fb4434486b70cb61d960bbd717268
Moshi Li <li.moshi95@gmail.com>
2023-01-24 18:42:25 -0500

    fix: remove legacy component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import "./assets/style/theme.css";
-import "./header-elem/header-elem";
-import "./nav-bar-elem/nav-bar-elem";
-import "./article-elem/article-elem";
-import "./about-elem/about-elem";
 import "./poke-card-elem/poke-card-elem";
 import "./poke-auction-elem/poke-auction-elem";
